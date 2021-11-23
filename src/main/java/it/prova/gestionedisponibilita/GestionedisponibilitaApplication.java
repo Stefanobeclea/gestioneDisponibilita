1dab12ff3641021d7912a15850efa1b681ea7f6f
Stefano Beclea <stefaniulianbeclea@gmail.com>
2021-11-23 10:00:49 +0100

    Creato Progetto

---
+++ src/main/java/it/prova/gestionedisponibilita/GestionedisponibilitaApplication.java
@@ -0,0 +1,13 @@
+package it.prova.gestionedisponibilita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionedisponibilitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionedisponibilitaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/prova/gestionedisponibilita/GestionedisponibilitaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.prova.gestionedisponibilita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionedisponibilitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
