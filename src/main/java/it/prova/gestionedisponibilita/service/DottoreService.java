2f1a3ab63b960343406c4641f48b42cb87f54139
Stefano Beclea <stefaniulianbeclea@gmail.com>
2021-11-23 10:52:46 +0100

    implemenentato service Dottore (Only Crud)

--- src/main/java/it/prova/gestionedisponibilita/service/DottoreService.java
+++ src/main/java/it/prova/gestionedisponibilita/service/DottoreService.java
@@ -1,5 +1,18 @@
 package it.prova.gestionedisponibilita.service;
 
+import java.util.List;
+
+import it.prova.gestionedisponibilita.model.Dottore;
+
 public interface DottoreService {
+	List<Dottore> listAllElements();
+
+	Dottore caricaSingoloElemento(Long id);
+
+	Dottore aggiorna(Dottore dottoreInstance);
+
+	Dottore inserisciNuovo(Dottore dottoreInstance);
+
+	void rimuovi(Dottore dottoreInstance);
 
 }
--- src/main/java/it/prova/gestionedisponibilita/service/DottoreServiceImpl.java
+++ src/main/java/it/prova/gestionedisponibilita/service/DottoreServiceImpl.java
@@ -1,5 +1,43 @@
 package it.prova.gestionedisponibilita.service;
 
-public class DottoreServiceImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import it.prova.gestionedisponibilita.model.Dottore;
+import it.prova.gestionedisponibilita.repository.DottoreRepository;
+
+@Service
+public class DottoreServiceImpl implements DottoreService{
+
+	@Autowired
+	DottoreRepository repository;
+	
+	@Override
+	public List<Dottore> listAllElements() {
+		return (List<Dottore>)repository.findAll();
+	}
+
+	@Override
+	public Dottore caricaSingoloElemento(Long id) {
+		return repository.findById(id).orElse(null);
+	}
+
+	@Override
+	public Dottore aggiorna(Dottore dottoreInstance) {
+		return repository.save(dottoreInstance);
+	}
+
+	@Override
+	public Dottore inserisciNuovo(Dottore dottoreInstance) {
+		return repository.save(dottoreInstance);
+	}
+
+	@Override
+	public void rimuovi(Dottore dottoreInstance) {
+		repository.delete(dottoreInstance);
+		
+	}
 
 }
